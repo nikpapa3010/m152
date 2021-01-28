c2dbfb35cad9dbd858b1413c34a6ed15a1f1901b
Niko <nikpapa3010@gmail.com>
2021-01-28 10:15:01 +0100

    added background

--- Project/index.php
+++ Project/index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <html>
 <head>
     <!-- Required meta tags -->
@@ -12,16 +9,24 @@
 
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    
-</head>
+    <style>
+body {
+  background-image: url('./Pictures/Logo.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+  
+}
+
+</style>
+  </head>
 <body>
     <div class="container">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand mr-auto" href="#">
+        <a class="navbar-brand mr-auto" href="">
           <img src="./Pictures/Logo.png" width="50" height="50" href="." alt="">
         </a>
 
@@ -38,15 +43,7 @@
              <a class="nav-link" href="">Projekte</a>
             </li>
           </ul>
-          <ul class="navbar-nav mr-0 my-lg-0">
-            <li class="nav-item dropdown">
-             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Test  
-             </a>
-             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Dashboard</a>
-            </li>
-          </ul>
+        
       </nav>
     </div>
 
