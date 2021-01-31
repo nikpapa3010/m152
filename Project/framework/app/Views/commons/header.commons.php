e7c28c19b3e2d12d3a3d33830f7d7b7b1b1cd4ff
Niko <60187035+nikpapa3010@users.noreply.github.com>
2021-01-31 20:40:18 +0100

    header updated

--- Project/framework/app/Views/commons/header.commons.php
+++ Project/framework/app/Views/commons/header.commons.php
@@ -1,7 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <img src="public/img/Logo.png" width="50" height="50" class="d-inline-block align-top" alt="">
-    <a class="navbar-brand" href="">M.E.P</a>
+    <img src="public/img/Logo.png" width="50" height="50" class="d-inline-block align-top" alt="" >
+    <a class="navbar-brand" href=".">M.E.P</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
