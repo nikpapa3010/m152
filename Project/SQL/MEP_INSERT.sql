7b991575bb2769f4a11920db55bd059ef4d8be0e
Niko <60187035+nikpapa3010@users.noreply.github.com>
2021-01-31 20:34:51 +0100

    Dokumentation writen, Database updated

--- Project/SQL/MEP_INSERT.sql
+++ Project/SQL/MEP_INSERT.sql
@@ -1,5 +1,7 @@
 INSERT INTO Usertype (Usertype)
 VALUES("Admin");
+INSERT INTO Usertype (Usertype)
+VALUES ("User");
 
 INSERT INTO User (Username, Email, Password, UsertypeID, MailingList)
 VALUES("Beep", "beepboop", "Boop", 1, 0);
@@ -10,6 +12,20 @@ INSERT INTO Artikel (Title, Text, Date, UserID)
 VALUES("Second", "Something else to write about", NOW(), 1);
 INSERT INTO Artikel (Title, Text, Date, UserID)
 VALUES("Third", "Yet more to write about", NOW(), 1);
+
+INSERT INTO Video (VideoName, VideoDesc, Video)
+VALUES("Verfallend | Sneak Peek Trailer", "First sneak peek at Verfallend, the upcoming game by the Miskatonic Explorer Proprietorship
+
+
+Official Discord Server
+https://discord.gg/tfCDGbw", "vySO9Q8oOSU");
+
+INSERT INTO Projekt (Projektname, Synopsis, VideoID)
+VALUES ("Verfallend", "A game about flying ships or something.",1);
+
+CREATE VIEW v_Project AS
+SELECT P.Projektname, P.Synopsis, V.VideoName, V.VideoDesc, V.Video FROM Projekt AS P
+LEFT JOIN Video AS V ON V.VideoID = P.VideoID;
 
 CREATE VIEW v_LatestArtikel AS
 SELECT Artikel.Title, Artikel.Text, Artikel.Date, User.Username FROM Artikel 
@@ -28,3 +44,5 @@ ORDER BY Date ASC LIMIT 4 OFFSET 1;
 
 SELECT * FROM v_LatestArtikel;
 SELECT * FROM v_LastArtikel;
+SELECT * FROM v_Project;
+SELECT * FROM User;
